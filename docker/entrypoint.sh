4402acadd487eaa87a2a9e8593e2e23400caf431
Luiz de Jesus <luiz.dejesus@jolibrain.com>
2025-10-06 11:27:52 +0200

    fix to user with proper user ownership (and not root)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+# Set HOME to a writable directory
+export HOME=/tmp
+
+# Configure git to trust /app directory
+git config --global --add safe.directory /app
+
 # always log in
 huggingface-cli login --token "$HF_TOKEN"
 
